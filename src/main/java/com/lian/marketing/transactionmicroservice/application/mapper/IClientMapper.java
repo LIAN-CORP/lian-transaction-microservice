1e89d1aab41ed2ba496d93df49bac92736cadd35
3lPr1mo <erickenriquechaparro@gmail.com>
2025-06-29 14:46:33 -0500

    refactor(app): refactor mapper annotation adding component model spring

--- src/main/java/com/lian/marketing/transactionmicroservice/application/mapper/IClientMapper.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/application/mapper/IClientMapper.java
@@ -5,7 +5,7 @@ import com.lian.marketing.transactionmicroservice.domain.model.Client;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface IClientMapper {
     @Mapping(ignore = true, target = "id")
     Client toModel(CreateClientRequest createClientRequest);
