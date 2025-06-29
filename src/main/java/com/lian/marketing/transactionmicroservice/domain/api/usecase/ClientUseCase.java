892f4ab256f31b1e7bf5db253b18615265c84c95
3lPr1mo <erickenriquechaparro@gmail.com>
2025-06-29 13:34:25 -0500

    refactor(domain): add slf4j loombok annotation

--- src/main/java/com/lian/marketing/transactionmicroservice/domain/api/usecase/ClientUseCase.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/domain/api/usecase/ClientUseCase.java
@@ -5,15 +5,13 @@ import com.lian.marketing.transactionmicroservice.domain.constants.GeneralConsta
 import com.lian.marketing.transactionmicroservice.domain.model.Client;
 import com.lian.marketing.transactionmicroservice.domain.spi.IClientPersistencePort;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.UUID;
 
+@Slf4j
 @RequiredArgsConstructor
 public class ClientUseCase implements IClientServicePort {
-
-    private static final Logger logger = LoggerFactory.getLogger(ClientUseCase.class);
 
     private final IClientPersistencePort clientPersistencePort;
 
@@ -21,7 +19,7 @@ public class ClientUseCase implements IClientServicePort {
     public void saveClient(Client client) {
         if(client.getPhone().length() < 10) {
             client.setPhone(GeneralConstants.COLOMBIA_PREFIX + client.getPhone());
-            logger.info("Phone number has no prefix, adding prefix to +57");
+            log.info("Phone number has no prefix, adding prefix to +57");
         }
         client.setId(UUID.randomUUID());
         clientPersistencePort.saveClient(client);
