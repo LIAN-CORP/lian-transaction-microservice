245646e308c94a5f3e588ca4724ab187d5ed8d24
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-09 18:44:12 -0500

    refactor(domain): add save transaction constat to sfl4j

--- src/main/java/com/lian/marketing/transactionmicroservice/domain/constants/GeneralConstants.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/domain/constants/GeneralConstants.java
@@ -6,4 +6,5 @@ public class GeneralConstants {
     public static final String COLOMBIA_PREFIX = "+57";
     public static final String PHONE_REGEX = "^\\+57[0-9]{9}$";
     public static final String USER_DO_NOT_EXISTS = "User do not exists";
+    public static final String SAVING_TRANSACTION_SFL4J = "Saving transaction {}";
 }
