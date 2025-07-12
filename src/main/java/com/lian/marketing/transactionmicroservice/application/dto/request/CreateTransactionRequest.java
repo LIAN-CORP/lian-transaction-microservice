0ef50e1c307e9692fd6062f3c669a176c858899f
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-12 13:00:38 -0500

    feat(app): add validation annotations to CreateTransactionRequest and integrate CreateClientRequest

--- src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CreateTransactionRequest.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CreateTransactionRequest.java
@@ -1,11 +1,15 @@
 package com.lian.marketing.transactionmicroservice.application.dto.request;
 
+import com.lian.marketing.transactionmicroservice.application.constants.ConstantDto;
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.UUID;
 
 public record CreateTransactionRequest (
+        @NotEmpty(message = ConstantDto.TRANSACTION_TYPE_MOVEMENT_MUST_BE_NOT_EMPTY)
         String typeMovement,
-        String transactionDate,
-        String clientId,
+        CreateClientRequest client,
+        @NotEmpty(message = ConstantDto.USER_ID_MUST_BE_NOT_EMPTY)
         UUID userId
 ) {
 }
