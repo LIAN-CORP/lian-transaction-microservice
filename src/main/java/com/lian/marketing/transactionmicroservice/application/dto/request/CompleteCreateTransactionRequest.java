2732cf989a92f4380c1971baabedc89badf7e5b7
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-15 08:50:02 -0500

    feat(app): add CompleteCreateTransactionRequest with validation annotations

---
+++ src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CompleteCreateTransactionRequest.java
@@ -0,0 +1,14 @@
+package com.lian.marketing.transactionmicroservice.application.dto.request;
+
+import com.lian.marketing.transactionmicroservice.application.constants.ConstantDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
+
+public record CompleteCreateTransactionRequest(
+        @Valid
+        CreateTransactionRequest transaction,
+        @NotEmpty(message = ConstantDto.PRODUCT_LIST_MUST_BE_NOT_EMPTY)
+        List<ProductQuantityRequest> products
+) {
+}
