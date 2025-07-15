d7d6d07c8917a8465948508fca979d46070b6588
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-15 09:22:14 -0500

    feat(domain): add `createCompleteTransaction` method to `ITransactionServicePort`

--- src/main/java/com/lian/marketing/transactionmicroservice/domain/api/ITransactionServicePort.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/domain/api/ITransactionServicePort.java
@@ -1,8 +1,10 @@
 package com.lian.marketing.transactionmicroservice.domain.api;
 
+import com.lian.marketing.transactionmicroservice.domain.model.CompleteTransaction;
 import com.lian.marketing.transactionmicroservice.domain.model.Transaction;
 import reactor.core.publisher.Mono;
 
 public interface ITransactionServicePort {
     Mono<Void> createTransaction(Transaction transaction);
+    Mono<Void> createCompleteTransaction(CompleteTransaction completeTransaction);
 }
