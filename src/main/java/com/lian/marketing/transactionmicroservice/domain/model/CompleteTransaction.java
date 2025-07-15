31c5bf086d52316e47a0483dcedf9b969c459b78
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-15 08:56:55 -0500

    feat(domain): add CompleteTransaction model with Lombok annotations

---
+++ src/main/java/com/lian/marketing/transactionmicroservice/domain/model/CompleteTransaction.java
@@ -0,0 +1,14 @@
+package com.lian.marketing.transactionmicroservice.domain.model;
+
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@ToString
+public class CompleteTransaction {
+    private Transaction transaction;
+    private List<ProductTransaction> productTransactions;
+}
