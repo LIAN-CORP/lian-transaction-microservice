6f4ef30ee8247990d226dd4c81d6ac184ad68c10
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-15 08:49:48 -0500

    feat(app): add `@Valid` annotation to `CreateTransactionRequest` client field

--- src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CreateTransactionRequest.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CreateTransactionRequest.java
@@ -1,6 +1,7 @@
 package com.lian.marketing.transactionmicroservice.application.dto.request;
 
 import com.lian.marketing.transactionmicroservice.application.constants.ConstantDto;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 public record CreateTransactionRequest (
         @NotEmpty(message = ConstantDto.TRANSACTION_TYPE_MOVEMENT_MUST_BE_NOT_EMPTY)
         String typeMovement,
+        @Valid
         CreateClientRequest client,
         @NotNull(message = ConstantDto.USER_ID_MUST_BE_NOT_EMPTY)
         UUID userId
