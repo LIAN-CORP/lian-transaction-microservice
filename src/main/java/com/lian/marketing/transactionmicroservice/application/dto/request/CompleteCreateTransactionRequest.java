07bcbf99a9c943263aed3728c51b55d5d93230b5
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-20 00:42:29 -0500

    refactor(app): remove `@Valid` annotation from `CompleteCreateTransactionRequest`

--- src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CompleteCreateTransactionRequest.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CompleteCreateTransactionRequest.java
@@ -1,12 +1,11 @@
 package com.lian.marketing.transactionmicroservice.application.dto.request;
 
 import com.lian.marketing.transactionmicroservice.application.constants.ConstantDto;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record CompleteCreateTransactionRequest(
-        @Valid
+        //@Valid
         CreateTransactionRequest transaction,
         @NotEmpty(message = ConstantDto.PRODUCT_LIST_MUST_BE_NOT_EMPTY)
         List<ProductTransactionRequest> products
