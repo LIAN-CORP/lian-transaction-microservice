87a75c773a596645839169900625ecdbd30286f8
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-20 00:44:02 -0500

    feat(app): add product stock discount logic in `TransactionAdapter`

--- src/main/java/com/lian/marketing/transactionmicroservice/infrastructure/driven/r2dbc/postgres/adapter/TransactionAdapter.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/infrastructure/driven/r2dbc/postgres/adapter/TransactionAdapter.java
@@ -2,15 +2,18 @@ package com.lian.marketing.transactionmicroservice.infrastructure.driven.r2dbc.p
 
 import com.lian.marketing.transactionmicroservice.domain.constants.GeneralConstants;
 import com.lian.marketing.transactionmicroservice.domain.model.ExistsResponse;
+import com.lian.marketing.transactionmicroservice.domain.model.ProductTransaction;
 import com.lian.marketing.transactionmicroservice.domain.model.Transaction;
 import com.lian.marketing.transactionmicroservice.domain.spi.ITransactionPersistencePort;
 import com.lian.marketing.transactionmicroservice.infrastructure.driven.r2dbc.postgres.mapper.ITransactionEntityMapper;
 import com.lian.marketing.transactionmicroservice.infrastructure.driven.r2dbc.postgres.repository.TransactionRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -21,13 +24,17 @@ public class TransactionAdapter implements ITransactionPersistencePort {
 
     @Qualifier("userWebClient")
     private final WebClient userWebClient;
+    @Qualifier("productWebClient")
+    private final WebClient productWebClient;
 
     public TransactionAdapter(TransactionRepository transactionRepository,
                               ITransactionEntityMapper transactionEntityMapper,
-                              @Qualifier("userWebClient") WebClient userWebClient) {
+                              @Qualifier("userWebClient") WebClient userWebClient,
+                              @Qualifier("productWebClient") WebClient productWebClient) {
         this.transactionRepository = transactionRepository;
         this.transactionEntityMapper = transactionEntityMapper;
         this.userWebClient = userWebClient;
+        this.productWebClient = productWebClient;
     }
 
     @Override
@@ -45,6 +52,17 @@ public class TransactionAdapter implements ITransactionPersistencePort {
                 .bodyToMono(ExistsResponse.class)
                 .map(ExistsResponse::exists)
                 .doOnNext(exists -> log.info("User exists: {}", exists));
+    }
+
+    @Override
+    public Mono<Void> discountProductStock(List<ProductTransaction> productTransactions) {
+        return productWebClient.post()
+                .uri("/product/discount")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(productTransactions)
+                .retrieve()
+                .toBodilessEntity()
+                .then();
     }
 
 }
