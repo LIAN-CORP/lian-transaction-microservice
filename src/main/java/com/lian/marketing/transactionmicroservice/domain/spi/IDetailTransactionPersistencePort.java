d8121f32da494b72eba169852e76458de7db016a
3lPr1mo <erickenriquechaparro@gmail.com>
2025-07-25 21:53:05 -0500

    refactor(domain): remove unused imports from `IDetailTransactionPersistencePort` interface

--- src/main/java/com/lian/marketing/transactionmicroservice/domain/spi/IDetailTransactionPersistencePort.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/domain/spi/IDetailTransactionPersistencePort.java
@@ -1,10 +1,8 @@
 package com.lian.marketing.transactionmicroservice.domain.spi;
 
 import com.lian.marketing.transactionmicroservice.domain.model.DetailTransaction;
-import com.lian.marketing.transactionmicroservice.domain.model.ProductTransaction;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface IDetailTransactionPersistencePort {
