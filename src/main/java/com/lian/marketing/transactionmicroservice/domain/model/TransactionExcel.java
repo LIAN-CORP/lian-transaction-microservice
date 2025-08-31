d3d1a17474a4745e406f391c75496a485640cf5d
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-31 11:50:24 -0500

    feat(domain, config, db): add `TransactionExcel` model, update Flyway config, and refine migration
    - Introduced `TransactionExcel` model for handling transaction data in Excel format.
    - Enabled `spring.flyway.out-of-order` in application properties for flexible migration execution.
    - Updated `V3_4__ADD_USERID_COLUMN_TRANSACTION.sql` to add `user_id` column only if it does not already exist.

---
+++ src/main/java/com/lian/marketing/transactionmicroservice/domain/model/TransactionExcel.java
@@ -0,0 +1,13 @@
+package com.lian.marketing.transactionmicroservice.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter @Getter
+public class TransactionExcel {
+    private String transactionDate;
+}
