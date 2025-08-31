d27b133e57106c3f1d9e58ab72713e354ef19e7b
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-31 10:57:51 -0500

    feat(config): add CORS configuration for transaction microservice
    - Enabled CORS support with `CorsConfiguration` class.
    - Allowed all headers, methods, and credentials from `http://localhost:3000`.

---
+++ src/main/java/com/lian/marketing/transactionmicroservice/configuration/CorsConfiguartion.java
@@ -0,0 +1,19 @@
+package com.lian.marketing.transactionmicroservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsConfiguartion implements WebFluxConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
