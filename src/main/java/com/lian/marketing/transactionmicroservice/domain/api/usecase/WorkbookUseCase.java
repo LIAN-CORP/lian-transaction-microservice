51bc8a4fdbac828b69f1d4e38cfd10f108eddb85
3lPr1mo <erickenriquechaparro@gmail.com>
2025-08-31 14:33:18 -0500

    refactor(domain): make `WorkbookUseCase` reactive with `Mono` and `Flux`
    - Updated `generateWorkbook` to return a `Mono` instead of directly returning `ExcelReport`.
    - Refactored `generateTransactionSheet` and `generateDebtSheet` methods to be reactive using `Mono<Void>` and `Flux`.
    - Modified `IWorkbookServicePort` to accommodate the reactive return type.

--- src/main/java/com/lian/marketing/transactionmicroservice/domain/api/usecase/WorkbookUseCase.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/domain/api/usecase/WorkbookUseCase.java
@@ -18,6 +18,7 @@ import org.dhatim.fastexcel.Worksheet;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -30,86 +31,96 @@ public class WorkbookUseCase implements IWorkbookServicePort {
     private final IWorkbookPersistencePort workbookPersistencePort;
 
     @Override
-    public ExcelReport generateWorkbook(LocalDate start, LocalDate end) {
-        try(
-                ByteArrayOutputStream os = new ByteArrayOutputStream();
-                Workbook wb = new Workbook(os, "LIAN", "1.0")
-        ){
-            Worksheet transactionSheet = wb.newWorksheet("Transacciones");
-            Worksheet detailTransactionSheet = wb.newWorksheet("Detalle de Transacciones");
-            Worksheet debtSheet = wb.newWorksheet("Deudas");
+    public Mono<ExcelReport> generateWorkbook(LocalDate start, LocalDate end) {
+        return Mono.using(
+          ByteArrayOutputStream::new,
+          os -> {
+              Workbook wb = new Workbook(os, "LIAN", "1.0");
+              Worksheet transactionSheet = wb.newWorksheet("Transacciones");
+              Worksheet detailTransactionSheet = wb.newWorksheet("Detalle de Transacciones");
+              Worksheet debtSheet = wb.newWorksheet("Deudas");
 
-            generateTransactionSheet(transactionSheet, detailTransactionSheet, start, end);
-            generateDebtSheet(debtSheet, start, end);
-
-            DataBuffer buffer = new DefaultDataBufferFactory().wrap(os.toByteArray());
-            String fileName = start.toString().substring(0, 10).replace("-", "")
-                    + "_" + end.toString().substring(0, 10).replace("-", "") + ".xlsx";
-            return new ExcelReport(fileName, Flux.just(buffer));
-        } catch (IOException e) {
-            log.error(GeneralConstants.ERROR_CREATING_EXCEL_REPORT_SFL4J, e.getMessage());
-            return new ExcelReport(
-                    "error.xlsx",
-                    Flux.error(new ErrorCreatingExcelReportException(GeneralConstants.ERROR_CREATING_EXCEL_REPORT_SFL4J))
-            );
-        }
+              return generateTransactionSheet(transactionSheet, detailTransactionSheet, start, end)
+                .then(generateDebtSheet(debtSheet, start, end))
+                .then(Mono.fromCallable(() -> {
+                    wb.close();
+                    String fileName = start.toString().substring(0, 10).replace("-", "")
+                      + "_" + end.toString().substring(0, 10).replace("-", "") + ".xlsx";
+                    DataBuffer buffer = new DefaultDataBufferFactory().wrap(os.toByteArray());
+                    return new ExcelReport(fileName, Flux.just(buffer));
+                }));
+          },
+          os -> {
+            try { os.close(); } catch (IOException e) {
+              throw new ErrorCreatingExcelReportException(GeneralConstants.ERROR_CREATING_EXCEL_REPORT_SFL4J);
+            }
+          }
+        );
     }
 
-    private void generateTransactionSheet(Worksheet ws, Worksheet wsDetail, LocalDate start, LocalDate end) {
-        //HEADERS
-        for(int i = 0; i < TransactionReportEnum.values().length; i++){
-            ws.value(0,i, TransactionReportEnum.values()[i].getColumnName());
+    private Mono<Void> generateTransactionSheet(Worksheet ws, Worksheet wsDetail, LocalDate start, LocalDate end) {
+        // HEADERS
+        for (int i = 0; i < TransactionReportEnum.values().length; i++) {
+            ws.value(0, i, TransactionReportEnum.values()[i].getColumnName());
         }
 
-        for(int i = 0; i < DetailTransactionReportEnum.values().length; i++){
-            wsDetail.value(0,i, DetailTransactionReportEnum.values()[i].getColumnName());
+        for (int i = 0; i < DetailTransactionReportEnum.values().length; i++) {
+            wsDetail.value(0, i, DetailTransactionReportEnum.values()[i].getColumnName());
         }
 
-        //DATA
-        workbookPersistencePort.findTransactionReportsByDate(start, end).collectList()
-                .subscribe(transactions -> {
-                    int i = 0;
-                    int j = 0;
-                    for(TransactionReport t : transactions){
-                        ws.value(i, 0, t.getTransactionId());
-                        ws.value(i, 1, t.getTypeMovement());
-                        ws.value(i, 2, t.getTransactionDate());
-                        for(DetailTransactionReport d : t.getDetailTransactionReports()){
-                            wsDetail.value(j, 0, d.getDetailTransactionId());
-                            wsDetail.value(j, 1, d.getClientName());
-                            wsDetail.value(j, 2, d.getClientPhone());
-                            wsDetail.value(j, 3, d.getProductName());
-                            wsDetail.value(j, 4, d.getUnitPrice());
-                            wsDetail.value(j, 5, d.getQuantity());
-                            wsDetail.value(j, 6, d.getTotalPrice());
-                            wsDetail.value(j, 7, t.getTransactionId());
-                            j++;
-                        }
-                    }
-                });
+        // DATA
+        return workbookPersistencePort.findTransactionReportsByDate(start, end)
+          .index()
+          .flatMap(tuple -> {
+              long i = tuple.getT1() + 1;
+              TransactionReport t = tuple.getT2();
+
+              ws.value((int) i, 0, t.getTransactionId());
+              ws.value((int) i, 1, t.getTypeMovement());
+              ws.value((int) i, 2, t.getTransactionDate());
+
+              return Flux.fromIterable(t.getDetailTransactionReports())
+                .index()
+                .doOnNext(detailTuple -> {
+                    long j = detailTuple.getT1() + 1;
+                    DetailTransactionReport d = detailTuple.getT2();
+
+                    wsDetail.value((int) j, 0, d.getDetailTransactionId());
+                    wsDetail.value((int) j, 1, d.getClientName());
+                    wsDetail.value((int) j, 2, d.getClientPhone());
+                    wsDetail.value((int) j, 3, d.getProductName());
+                    wsDetail.value((int) j, 4, d.getUnitPrice());
+                    wsDetail.value((int) j, 5, d.getQuantity());
+                    wsDetail.value((int) j, 6, d.getTotalPrice());
+                    wsDetail.value((int) j, 7, t.getTransactionId());
+                })
+                .then();
+          })
+          .then();
     }
 
-    private void generateDebtSheet(Worksheet ws, LocalDate start, LocalDate end) {
-        //HEADERS
-        for(int i = 0; i < DebtReportEnum.values().length; i++){
-            ws.value(0,i, DebtReportEnum.values()[i].getColumnName());
-        }
-        //DATA
-        workbookPersistencePort.findDebtReportsByDate(start, end)
-          .collectList()
-          .subscribe(debts -> {
-              int i = 0;
-              for(DebtReport debt : debts){
-                  ws.value(i, 0, debt.getDebtId());
-                  ws.value(i, 1, debt.getClientName());
-                  ws.value(i, 2, debt.getTotalAmount());
-                  ws.value(i, 3, debt.getTotalPaid());
-                  ws.value(i, 4, debt.getStatus());
-                  ws.value(i, 5, debt.getCreatedAt());
-                  ws.value(i, 6, debt.getUpdatedAt());
-                  i++;
-              }
-          });
-    }
 
+    private Mono<Void> generateDebtSheet(Worksheet ws, LocalDate start, LocalDate end) {
+        // HEADERS
+        for (int i = 0; i < DebtReportEnum.values().length; i++) {
+            ws.value(0, i, DebtReportEnum.values()[i].getColumnName());
+        }
+
+        // DATA
+        return workbookPersistencePort.findDebtReportsByDate(start, end)
+          .index()
+          .doOnNext(tuple -> {
+              long row = tuple.getT1() + 1;
+              DebtReport debt = tuple.getT2();
+
+              ws.value((int) row, 0, debt.getDebtId());
+              ws.value((int) row, 1, debt.getClientName());
+              ws.value((int) row, 2, debt.getTotalAmount());
+              ws.value((int) row, 3, debt.getTotalPaid());
+              ws.value((int) row, 4, debt.getStatus());
+              ws.value((int) row, 5, debt.getCreatedAt());
+              ws.value((int) row, 6, debt.getUpdatedAt());
+          })
+          .then();
+    }
 }
