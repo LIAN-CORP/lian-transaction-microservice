1f8ceeef215d37a9f18555d38e43176af616c299
3lPr1mo <erickenriquechaparro@gmail.com>
2025-12-28 11:34:00 -0500

    refactor(domain): remove unused userId field and validation from CreateTransactionRequest

--- src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CreateTransactionRequest.java
+++ src/main/java/com/lian/marketing/transactionmicroservice/application/dto/request/CreateTransactionRequest.java
@@ -11,8 +11,8 @@ public record CreateTransactionRequest (
         @NotEmpty(message = ConstantDto.TRANSACTION_TYPE_MOVEMENT_MUST_BE_NOT_EMPTY)
         String typeMovement,
         @Valid
-        CreateClientRequest client,
-        @NotNull(message = ConstantDto.USER_ID_MUST_BE_NOT_EMPTY)
-        UUID userId
+        CreateClientRequest client
+        //@NotNull(message = ConstantDto.USER_ID_MUST_BE_NOT_EMPTY)
+        //UUID userId
 ) {
 }
